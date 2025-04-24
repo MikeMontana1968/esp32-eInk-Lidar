7c7ace6dce6cde0afa62a41589237067532bdce8
MikeMontana1968 <mikemontana@hotmail.com>
2025-04-23 20:43:09 -0400

    Ready for test on Victory motorcycle

---
+++ common.h
@@ -0,0 +1,216 @@
+extern float STD_DEV;
+extern float AVG_MM;
+extern Adafruit_VL6180X vlx;
+
+#include ".credentials.h"
+
+struct tm timeinfo;
+
+// NTP server details
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;  // Offset for GMT in seconds
+const int daylightOffset_sec = -4 * 60 * 60;  // Daylight savings time in seconds
+
+// void drawBorder(uint x, uint y, uint w, uint h, uint thickness = 4, uint color = BLACK) {
+//   for(uint i = thickness; i > 0; i--){
+//     display.drawRect(x, y, w, h, color);
+//     x++; y++; w-=2; h-=2;
+//   }
+// }
+// void user_interactive_config() {
+//     paint("CONFIG");
+//     // rtc.setTime(input_long("Enter current unix epoch:"));
+//     // rtc.offset = -4 * 60;
+//     FULL_TANK_MM = input_long("Enter Tank depth in mm (eg 127)");
+//     EMPTY_TANK_MM = input_long("Enter mm to full-line of tank in mm (eg 10)");
+//     TANK_GALLONS = input_float("Enter tank capacity in gallons (eg 3)");
+// }
+
+float getMean(uint8_t* val, int arrayCount) {
+  long total = 0;
+  float avg = 0;
+  Serial.printf("getMean([], %i) = \n", arrayCount);
+  for (int i = 0; i < arrayCount; i++) {
+    total = total + val[i];
+  }
+  if(total)
+    avg = total/(float)arrayCount;
+
+  Serial.print("=" + String(avg, 2) + "\n");
+  return avg;
+}
+
+float getStdDev(uint8_t* val, int arrayCount) {
+  float avg = getMean(val, arrayCount);
+  float variance = 0;
+  long total = 0;
+  float stdDev = 0;
+  for (int i = 0; i < arrayCount; i++) {
+    
+    total = total + (val[i] - avg) * (val[i] - avg);
+  }
+  if(total)
+    variance = total/(float)arrayCount;
+    stdDev = sqrt(variance);
+  Serial.print("getStdDev([], " + String(arrayCount) + ")=" + String(stdDev,2) + "\n");
+  return stdDev;
+}
+
+long input_long(String prompt) {
+    Serial.println(prompt);
+    while (Serial.available()) {
+      Serial.read();
+    }
+    while(Serial.available() == 0){}
+    return Serial.parseInt();
+}
+
+long input_float(String prompt) {
+    Serial.println(prompt);
+    while (Serial.available()) {
+      Serial.read();
+    }
+    while(Serial.available() == 0){}
+    return Serial.parseFloat();
+}
+
+float read_lux() {
+  float lux = vlx.readLux(VL6180X_ALS_GAIN_5);
+  Serial.print("Lux: "); Serial.println(lux);
+  return lux;
+}
+
+uint8_t read_vlx() {
+  uint8_t range = vlx.readRange();
+  uint8_t status = vlx.readRangeStatus();
+
+  if (status == VL6180X_ERROR_NONE) {
+    return range;
+  }
+
+  // Some error occurred, print it out!
+  
+  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+    Serial.println("System error");
+  }
+  else if (status == VL6180X_ERROR_ECEFAIL) {
+    Serial.println("ECE failure");
+  }
+  else if (status == VL6180X_ERROR_NOCONVERGE) {
+    Serial.println("No convergence");
+  }
+  else if (status == VL6180X_ERROR_RANGEIGNORE) {
+    Serial.println("Ignoring range");
+  }
+  else if (status == VL6180X_ERROR_SNR) {
+    Serial.println("Signal/Noise error");
+  }
+  else if (status == VL6180X_ERROR_RAWUFLOW) {
+    Serial.println("Raw reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RAWOFLOW) {
+    Serial.println("Raw reading overflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEUFLOW) {
+    Serial.println("Range reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEOFLOW) {
+    Serial.println("Range reading overflow");
+  }
+
+  return 0;
+}
+
+uint8_t sample_vlx(uint sample_size = VLX_SAMPLE_SIZE, uint delay_ms_per_sample = MS_DELAY_PER_SAMPLE) {
+  Serial.printf("sample_vlx(sample_size=%i, delay_ms_per_sample=%i)\n", sample_size, delay_ms_per_sample);
+  ulong start = millis();
+  uint8_t vlx_samples[sample_size+1];
+  memset(vlx_samples, sizeof(vlx_samples), 0);
+  uint8_t offset = int(sample_size * 0.20);
+  uint8_t i = sample_size;
+  while(i) {
+    uint8_t x = read_vlx();
+    if(x ) {
+      vlx_samples[i--] = x;
+      delay(delay_ms_per_sample);
+    }
+  }
+  sortArrayReverse(vlx_samples, sample_size);
+
+  uint sum = 0;  
+  uint included_sample_count = 0;
+  for(i = offset; i < (sample_size - offset); i++) {
+    sum += vlx_samples[i];
+    included_sample_count++;
+  }
+  float avg = sum / included_sample_count;
+  STD_DEV = getStdDev(vlx_samples, sample_size);
+  AVG_MM = avg;
+  Serial.print("sample_vlx(" + String(sample_size) + ")=" + String(avg,2));
+  Serial.printf(" max[%i,%i,%i]", vlx_samples[0], vlx_samples[1], vlx_samples[2]) ;
+  Serial.printf(" min[%i,%i,%i]", vlx_samples[sample_size-3], vlx_samples[sample_size-2], vlx_samples[sample_size-1]) ;
+  Serial.print(" stdDev " + String(STD_DEV,2)) ;
+  Serial.println();
+  return (uint)avg;
+}
+
+esp_sleep_wakeup_cause_t print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : 
+      Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); 
+      // if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
+      //   Serial.println("SPIFFS Mount Failed");
+      // } else {
+      //   readFile(SPIFFS, "/activity.log");
+      // }
+
+      //Serial.printf("RTC set to %s\n", rtc.getTime("%A, %B %d %Y %H:%M:%S"));
+      break;
+  }
+  return wakeup_reason;
+}
+
+void setupWiFi() {
+    WiFi.begin(ssid, password);  // Connect to WiFi
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected.");
+}
+
+void syncTime() {
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);  // Configure time with NTP server
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println("\nESP32 Time synchronized with NTP server.");
+  Serial.print("Current time: ");
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+
+    // disconnect WiFi
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
+
+  lastSyncMillis = millis();  // Record the last sync time in milliseconds
+}
+
+void checkTimeAndSync() {
+  // Check if 1 hour has passed since the last sync (1 hour = 3600000 milliseconds)
+  if (millis() - lastSyncMillis >= 3600000) {
+    Serial.println("Synchronizing time with NTP...");
+    syncTime();
+  }
+}
--- heltec-gas-gauge.ino
+++ heltec-gas-gauge.ino
@@ -1,28 +1,100 @@
-#include <heltec-eink-modules.h>
-#include "Fonts/FreeSans9pt7b.h"
-#include "Fonts/FreeSansBold9pt7b.h"
-
-
 /*
 Set Board Type to: Vision Master E290
 https://github.com/todd-herbert/heltec-eink-modules/blob/main/docs/WirelessPaper/wireless_paper.md
 
 */
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
-char BUFFER[255] = {0};
 
+#include <heltec-eink-modules.h>
+#include "Fonts/FreeSans9pt7b.h"
+#include "Fonts/FreeSansBold9pt7b.h"
+#include <WiFi.h>
+#include <time.h>
+#include <ArduinoSort.h> // https://github.com/emilv/ArduinoSort/blob/master/examples/SortArray/SortArray.ino
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+#include <driver/adc.h>
+
+#define I2C_SDA GPIO_NUM_39
+#define I2C_SCL GPIO_NUM_38
+
+#define VLX_SAMPLE_SIZE 100
+#define MS_DELAY_PER_SAMPLE 25
+
+Adafruit_VL6180X vlx = Adafruit_VL6180X();
 EInkDisplay_VisionMasterE290 display;
+ulong lastSyncMillis = 0;
+
+const int FULL_TANK_MM = 5;
+const int EMPTY_TANK_MM = 45;
+const float TANK_GALLONS = 4.5;
+const int MPG_AVG = 40;
+
+char BUFFER[512] = {0};
+float STD_DEV = 0;
+float AVG_MM = 0;
+int PCT_FULL = 0; 
+float F_PCT_FULL = 0.0;
+float GAL_REMAIN = 0;
+RTC_DATA_ATTR int bootCount = 0;
+
+#define BOX_WIDTH 192
+#define BOX_HEIGHT 90
+#define BOX_TOP 100
+
+#include "common.h"
+
+uint pollData() {
+    Serial.println("pollData()");
+  
+    sample_vlx();
+    PCT_FULL = map(AVG_MM, FULL_TANK_MM, EMPTY_TANK_MM, 1, 100); 
+    if(PCT_FULL < 0)
+    PCT_FULL = 0;
+
+    F_PCT_FULL = (.01 * PCT_FULL);
+    float GAL_REMAIN = TANK_GALLONS * F_PCT_FULL;
+
+    uint sleep_time_seconds = 5 * 60;
+    if(STD_DEV > 12) 
+        sleep_time_seconds = 20;
+
+    memset(BUFFER, 0, sizeof(BUFFER));
+    String b = "";
+    getLocalTime(&timeinfo);
+    strftime(BUFFER, sizeof(BUFFER), "%b %d %Y %H:%M:%S", &timeinfo);
+    b += String("Full  :") + String(PCT_FULL) + String("% gal ") + String(GAL_REMAIN, 1) + "\n";
+    b += String("Range :") + String(int(MPG_AVG * GAL_REMAIN)) + " miles\n";
+    b += String("Fuel Depth :") + String(AVG_MM, 0) + String("mm StdDev ") + String(STD_DEV, 2) + "\n";
+    b += String(BUFFER) + "\n";
+    //b += String("Batt  :") + String(read_battery(), 2) + "v\n";
+    //b += String("Boot  #") + String(bootCount) + " Zz " + String(sleep_time_seconds) + "s\n";
+    b.toCharArray(BUFFER, b.length() + 1);
+    Serial.println(BUFFER);
+
+    display.clearMemory();  // Start a new drawing
+    display.landscape();
+    display.setFont( &FreeSans9pt7b );
+    display.printCenter(BUFFER);
+    display.update();
+    return sleep_time_seconds;
+}
 
 void setup() {
 	Serial.begin(115200);
     delay(200);
     Serial.print("Running ");
-    Serial.println(__FILENAME__);
     Serial.print(" Version: ");
     Serial.println(__TIMESTAMP__);
-	
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+    print_wakeup_reason();
+    Wire.setPins(I2C_SDA, I2C_SCL);
+    vlx.begin();
+    setupWiFi();
+    syncTime();
+}
+
+void original_setup() {
     display.print("Hello, world!");
     display.update();
     
@@ -86,5 +158,7 @@ void setup() {
 }
 
 void loop() {
-
+//checkTimeAndSync();  // Check if 1 hour has passed and sync if necessary
+    pollData();
+    delay(30 * 1000);
 }
